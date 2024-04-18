8b030c49bf48ba6f3ed2e9aaf05c386a1da42493
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-04-18 18:57:03 +0300

    feat: modifier en meme temps deux elements de la liste

--- src/liste.py
+++ src/liste.py
@@ -5,5 +5,7 @@ def main():
     print(rakotoniaina_name)
     rakotoniaina_name.insert(2, "Joro")
     print(rakotoniaina_name)
+    rakotoniaina_name[2:4]=["le fils", "la fille"]
+    print(rakotoniaina_name)
 if __name__ == '__main__':
     main()

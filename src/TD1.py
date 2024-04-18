e47ef681ac481c02d0c2f90aa99328b4723075a0
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-04-18 16:34:36 +0300

    feat: do first exercise

---
+++ src/TD1.py
@@ -0,0 +1,7 @@
+wallet = int(input("Entrer le montant dans votre wallet: "))
+
+produitquelconque = 50
+
+wallet -= produitquelconque
+
+print("Apres l'acchat du produit d'une valeur de " + str(produitquelconque) + ", votre wallet contient desormais " + str(wallet) )

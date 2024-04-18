ed5316dda6d4b9de3964137565efa0d2765b0507
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-04-18 16:23:35 +0300

    feat: write fonction to collect my grade for example

--- src/collect.py
+++ src/collect.py
@@ -1,7 +1,11 @@
 def main():
-    # recolte de notes
-    note1 = input("Entre ta premiere note: ")
 
+    note1 = int(input("Entre ta premiere note: "))
+    note2 = int(input("Entre ta seconde note: "))
+    note3 = int(input("Entre ta troisieme note: "))
+    result = (note1 + note2+note3) / 3
+
+    print("Ta moyenne est de " +  str(result))
 
 if __name__ == '__main__':
     main()

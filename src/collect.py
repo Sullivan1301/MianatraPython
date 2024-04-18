fd75824a070ae60032e50b46842aeab08dc82b19
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-04-18 11:27:40 +0300

    Mianatra Python Sullivan

---
+++ src/collect.py
@@ -0,0 +1,6 @@
+def main():
+    # recolte de notes
+    note1 = input("Entre ta premiere note: ")
+
+if __name__ == '__main__':
+    main()
---
+++ src/variable.py
@@ -0,0 +1,8 @@
+def main():
+    username = "Joro"
+    age = 21
+
+    print("Son nom est " + username + " et il a " + str(age) + " ans")
+
+if __name__ == '__main__':
+    main()
